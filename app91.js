2b0c74ec78b524ae77e7503e54320d5ef31ea100
Shreyas Pagar <130757999+shreyaspagar@users.noreply.github.com>
2025-05-29 20:07:25 +0530

    Add files via upload

---
+++ app91.js
@@ -0,0 +1,28 @@
+let students = [
+    {
+    name:"shreyas",
+    marks:89,
+},
+
+{
+    name : "Aman",
+    marks : 85,
+},
+
+{
+   name : "Pooja",
+   marks : 96, 
+},
+];
+
+let gpa = students.map((el) => {
+    return el.marks / 10;
+});
+
+
+
+
+
+let num = [1,2,3,4,5];
+
+let double = num.map((el) =>{});
